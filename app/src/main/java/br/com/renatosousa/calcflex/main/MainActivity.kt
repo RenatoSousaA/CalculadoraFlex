ce863d17e1f739998d689a12f343bb5ac4ff0c3c
Logon Aluno <logonaluno@local.com>
2019-09-17 22:42:18 -0300

    Initial commit

---
+++ app/src/main/java/br/com/renatosousa/calcflex/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.renatosousa.calcflex.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.renatosousa.calcflex.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/br/com/renatosousa/calcflex/result/ResultActivity.kt
@@ -0,0 +1,13 @@
+package br.com.renatosousa.calcflex.result
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.renatosousa.calcflex.R
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
